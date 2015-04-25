b1c600e0715f7ad4e36915d5005bfb527a529872
vineetakhurana <vkhuran2@ncsu.edu>
2015-04-25 00:15:04 -0400

    images added

--- chaos.js
+++ chaos.js
@@ -12,41 +12,42 @@ var servers = [
 var chaos = [];
 var proxy_s = httpProxy.createServer();
 
-function removeServer()
+function removeServer()  //to remove server from a list of potential servers
 {
 	var len = servers.length;
-	console.log("length of servers:",len);
+	//console.log("length of servers:",len);
 	var index = Math.ceil((len/2)-1); //logic
 	chaos.push(servers[index]);
 	servers.splice(index,1);
 }
 
-function addServer()
+function addServer()  //to arbitrarily bring in one of the removed server
 {
 	if(chaos.length!=0)
 	{
 		//console.log("added now");
 		servers.push(chaos[0]);
-		console.log("this will be added:",chaos[0]);
+		//console.log("this will be added:",chaos[0]);
 		chaos.splice(0,1);
 	}
 }
 //setInterval(function() {getReq()},10000);
-setInterval(function() {removeServer()},8000);
-setInterval(function() {addServer()},10000);
+setInterval(function() {removeServer()},8000); //interval at which a server is removed
+setInterval(function() {addServer()},10000);  //interval at which a server is added
 
-
+//proxy listening on localhost:3000
 http.createServer(function(req,res){
-	var target = { target: servers.shift() };
-	console.log('balancing request to:',target);
+	var target = { target: servers.shift() };  //to ensure round robin selection
+	//console.log('balancing request to:',target);
 	proxy_s.web(req,res,target);
 	servers.push(target.target);
 }).listen(3000);
 
+//app to display the list of available servers as targets to choose from at any given instance of time
 http.createServer(function(req,res){
 
 	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write("<table align=center> <tr> <td align=center> <h3> Servers available &nbsp; &nbsp; </h3> </td> <td align=center> <h3> Time </h3> </td> </tr>");
+	res.write("<table align=center> <tr> <td align=center> <h3> Production instances &nbsp; &nbsp; </h3> </td> <td align=center> <h3> Time </h3> </td> </tr>");
 	for(var each in servers)
 	{
 		var time = new Date();
